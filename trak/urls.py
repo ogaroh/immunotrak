4394c44806e05e2a41cffd47fa4b5b00ba910f9c
ogaroh <ogaroerick1998@gmail.com>
2019-04-30 16:35:26 +0300

    home url routes

--- trak/urls.py
+++ trak/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
+from .views import homePageView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', homePageView, name='home'),
 ]
--- trak/views.py
+++ trak/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("ImmunoTrak Home.")
+def homePageView(request):
+    return HttpResponse('Home Page')
