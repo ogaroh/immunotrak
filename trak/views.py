6105ca99ed509c5dd88b011b6c144e7236301fea
ogaroh <ogaroerick1998@gmail.com>
2019-04-30 16:17:01 +0300

    home url routes

--- trak/views.py
+++ trak/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("ImmunoTrak Home.")
+
+
+def trak(request):
+    return HttpResponse("Login Page.")
