6f6fa6eb70a83c476982624fa43ac161606d78ea
ogaroh <ogaroerick1998@gmail.com>
2019-06-27 11:09:18 +0300

    tables

--- charts/urls.py
+++ charts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from . import views
 
-from .views import ChartData, ChartsView
-
+from .views import get_data, ChartsView, EverythingChartData, LocationChartData, ChildrenView, ParentView, VaccineView, LocationView
 urlpatterns = [
     path('', ChartsView.as_view(), name="charts"),
-    path('api/chart/data/', ChartData.as_view()),  # new
+    path('tables/children/', ChildrenView.as_view()),
+    path('tables/vaccines/', VaccineView.as_view()),
+    path('tables/parents/', ParentView.as_view()),
+    path('tables/locations/', LocationView.as_view()),
+
+    path('api/data/', get_data, name="api-data"),  # endpoint 1
+    path('api/chart/data/', EverythingChartData.as_view()),  # endpoint 2
+    path('api/location-chart/data/', LocationChartData.as_view()),  # endpoint 2
 ]
--- charts/views.py
+++ charts/views.py
@@ -1,25 +1,101 @@
+from django.core import serializers
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from django.shortcuts import render
-from django.views.generic import View
+from django.http import JsonResponse
+from django.views.generic import View, ListView
 
+# models
 from trak import models as trak_models
+from schedule import models as schedule_models
 
 
-class ChartData(APIView):
-    authentication_classes = []
-    permission_classes = []
+Vaccines = trak_models.Vaccine
+Parents = trak_models.Parent
+Children = trak_models.Kid
+Counties = trak_models.Location
 
-    def get(self, request, format=None):
-        data = {
-            "labels": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-            "data": [12, 19, 3, 5, 2, 3, 10],
-        }
-
-        return Response(data)
+Schedules = schedule_models.Schedule
 
 
+class ChildrenView(ListView):
+    model = trak_models.Kid
+    context_object_name = 'children_list'
+    template_name = 'tables.html'
+
+
+
+class ParentView(ListView):
+    model = trak_models.Parent
+    context_object_name = 'parent_list'
+    template_name = 'parent_list.html'
+
+
+class LocationView(ListView):
+    model = trak_models.Location
+    context_object_name = 'parent_list'
+    template_name = 'location_list.html'
+
+
+class VaccineView(ListView):
+    model = trak_models.Vaccine
+    context_object_name = 'parent_list'
+    template_name = 'vaccine_list.html'
+
+
+
+# queryset counts
+vaccines_qs_count = Vaccines.objects.all().count()
+parents_qs_count = Parents.objects.all().count()
+children_qs_count =  Children.objects.all().count()
+locations_qs_count  = Counties.objects.all().count()
+schedules_qs_count = Schedules.objects.all().count()
+
+# get a list of all locations and vaccines
+locations = serializers.serialize("json", Counties.objects.all())
 
 class ChartsView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'chart.html')
+
+
+def get_data(request, *args, **kwargs):
+    data = {
+        "vaccines": 7,
+        "kids": 15,
+    }
+    return JsonResponse(data)
+
+
+class EverythingChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+
+        labels = ['Vaccines', 'Children', 'Parents', 'Appointments', 'Locations']
+        default_items = [vaccines_qs_count, children_qs_count, parents_qs_count,schedules_qs_count, locations_qs_count]
+       
+        data = {
+                "labels": labels,
+                "default": default_items,
+        }
+        return Response(data)
+
+
+class LocationChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+
+        location_items = [locations]
+
+        location_data = {
+            "labels": location_items,
+            "default": location_items,
+        }
+        return Response(location_data)
+
